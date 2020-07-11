b0e215a785c9a28cbadfb21fef7318d632e7eaaf
MichalSagi <smitzi@gmail.com>
2020-07-11 23:14:16 +0300

    'final'

---
+++ main.js
@@ -0,0 +1,23 @@
+let people;
+
+const getPeople = function () {
+  $.ajax({
+    method: "GET",
+    url: `https://randomuser.me/api/?results=10&inc=gender,name,email,login,id,picture`,
+    success: function (data) {
+      people = data.results;
+      renderPeople();
+    },
+    error: function (xhr, text, error) {
+      console.log(text);
+    },
+  });
+};
+getPeople();
+const source = $("#people-template").html();
+const template = Handlebars.compile(source);
+
+const renderPeople = function () {
+  const someHTML = template(people);
+  $("#people-data").append(someHTML);
+};
